746b8ec86c7081b296ba05ae664479d7736df11c
jinalp1 <jinalpadhiar15@gmail.com>
2024-06-26 18:48:44 +0530

    Changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
+const mongo_uri = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/';
+
+mongoose.connect(mongo_uri , { useNewUrlParser: true, useUnifiedTopology: true 
 })
